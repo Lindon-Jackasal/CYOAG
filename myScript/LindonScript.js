1231b9353e6f3a62c4ed8052419b84b6f63f02d7
Lindon Jackasal <40514935@live.napier.ac.uk>
2021-03-27 10:21:18 -0400

    Updated LindonScript.js

--- myScript/LindonScript.js
+++ myScript/LindonScript.js
@@ -7,8 +7,12 @@ var ctx = canvas.getContext("2d");
        canvas.width = 1540;
        canvas.height = 760;
 
-var dx = 2;
-var dy = -2;
+var x = -150;
+var y = 100;
+var xb = -950;
+var yb = 300;
+var dx = 1.2;
+var dy = 0;
 var brickRowCount = 6;
 var brickColumnCount = 7;
 var brickWidth = 217;
@@ -16,8 +20,11 @@ var brickHeight = 45;
 var brickPadding = 2;
 var brickOffsetTop = 471;
 var brickOffsetLeft = 3;
-
 var bricks = [];
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+
 for(var c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
     for(var r=0; r<brickRowCount; r++) {
@@ -40,6 +47,7 @@ function drawBricks() {
             ctx.closePath();
         }
     }
+    requestAnimationFrame(drawBricks);
 }
 
 //This function creates an ellipse/Humpty Dumpty
@@ -67,39 +75,65 @@ function drawHumptyDumpty() {
    ctx.stroke();
    ctx.restore();
 
-/*  ctx.beginPath();
-    ctx.arc(750, 420, 85, 25.1, 1 * Math.PI);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-    ctx.stroke();
-    ctx.restore();
-*/
+   requestAnimationFrame(drawHumptyDumpty);
 }
 
 function drawClouds() {
+   //The first cloud
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
    ctx.beginPath();
    ctx.lineWidth = 1;
-   ctx.moveTo(200, 100);
-   ctx.bezierCurveTo(160, 120, 160, 170, 260, 170);
-   ctx.bezierCurveTo(280, 200, 350, 200, 370, 170);
-   ctx.bezierCurveTo(450, 170, 450, 140, 420, 120);
-   ctx.bezierCurveTo(460, 60, 400, 50, 370, 70);
-   ctx.bezierCurveTo(350, 30, 280, 40, 280, 70);
-   ctx.bezierCurveTo(230, 25, 180, 40, 200, 100);
+   ctx.moveTo(x, y);
+   ctx.bezierCurveTo(x - 40, y + 20, x - 40, y + 70, x + 60, y + 70);
+   ctx.bezierCurveTo(x + 80, y + 100, x + 150, y + 100, x + 170, y + 70);
+   ctx.bezierCurveTo(x + 250, y + 70, x + 250, y + 40, x + 220, y + 20);
+   ctx.bezierCurveTo(x + 260, y - 40, x + 200, y - 50, x + 170, y - 30);
+   ctx.bezierCurveTo(x + 150, y - 75, x + 80, y - 60, x + 80, y - 30);
+   ctx.bezierCurveTo(x + 30, y - 75, x - 20, y - 60, x, y);
+   ctx.fillStyle = "#DFF8FF";
+   ctx.fill();
    ctx.stroke();
    ctx.closePath();
+   x += dx;
+   y += dy;
+
+    if (Math.abs(x) >= 1600) {
+        x = -230;
+    }
+
+   //The second cloud
+   ctx.beginPath();
+   ctx.lineWidth = 1;
+   ctx.moveTo(xb, yb);
+   ctx.bezierCurveTo(xb - 40, yb + 20, xb - 40, yb + 70, xb + 60, yb + 70);
+   ctx.bezierCurveTo(xb + 80, yb + 100, xb + 150, yb + 100, xb + 170, yb + 70);
+   ctx.bezierCurveTo(xb + 250, yb + 70, xb + 250, yb + 40, xb + 220, yb + 20);
+   ctx.bezierCurveTo(xb + 260, yb - 40, xb + 200, yb - 50, xb + 170, yb - 30);
+   ctx.bezierCurveTo(xb + 150, yb - 75, xb + 80, yb - 60, xb + 80, yb - 30);
+   ctx.bezierCurveTo(xb + 30, yb - 75, xb - 20, yb - 60, xb, yb);
+   ctx.fillStyle = "#DFF8FF";
+   ctx.fill();
+   ctx.stroke();
+   ctx.closePath();
+   xb += dx;
+   yb += dy;
+
+    if (Math.abs(xb) >= 1600) {
+        xb = -230;
+    }
+
+requestAnimationFrame(drawClouds);
 
 }
 
 function draw() {
+    drawClouds();
     drawBricks();
     drawHumptyDumpty();
-    drawClouds();
-
 }
 
+requestAnimationFrame(draw);
 
-var interval = setInterval(draw, 10);
 
 
 
