bc38ac0ebcb63c981a4cb9d75efea59ce6dcb9c7
Lindon Jackasal <40514935@live.napier.ac.uk>
2021-03-27 14:15:52 -0400

    Updated LindonScript.js

--- myScript/LindonScript.js
+++ myScript/LindonScript.js
@@ -11,15 +11,17 @@ var x = -150;
 var y = 100;
 var xb = -950;
 var yb = 300;
+var xc = 1350;
+var yc = 420;
 var dx = 1.2;
 var dy = 0;
 var brickRowCount = 6;
 var brickColumnCount = 7;
-var brickWidth = 217;
+var brickWidth = 218;
 var brickHeight = 45;
 var brickPadding = 2;
 var brickOffsetTop = 471;
-var brickOffsetLeft = 3;
+var brickOffsetLeft = 0;
 var bricks = [];
 var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                             window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -78,6 +80,109 @@ function drawHumptyDumpty() {
    requestAnimationFrame(drawHumptyDumpty);
 }
 
+function drawBush() {
+       //drawBush
+       ctx.beginPath();
+       ctx.lineWidth = 1;
+       ctx.moveTo(xc, yc);
+       ctx.bezierCurveTo(xc - 40, yc + 20, xc - 40, yc + 70, xc + 60, yc + 70);
+       ctx.bezierCurveTo(xc + 80, yc + 100, xc + 150, yc + 100, xc + 170, yc + 70);
+       ctx.bezierCurveTo(xc + 250, yc + 70, xc + 250, yc + 40, xc + 220, yc + 20);
+       ctx.bezierCurveTo(xc + 260, yc - 40, xc + 200, yc - 50, xc + 170, yc - 30);
+       ctx.bezierCurveTo(xc + 150, yc - 75, xc + 80, yc - 60, xc + 80, yc - 30);
+       ctx.bezierCurveTo(xc + 30, yc - 75, xc - 20, yc - 60, xc, yc);
+       ctx.fillStyle = "#24C000";
+       ctx.fill();
+       ctx.stroke();
+       ctx.closePath();
+
+       ctx.beginPath();
+       ctx.rect(0, 290, 130, 220);
+       ctx.fillStyle = "#BF8E02";
+       ctx.fill();
+       ctx.stroke();
+       ctx.restore();
+
+       var xd = -50;
+       var yd = 225;
+       ctx.beginPath();
+       ctx.lineWidth = 1;
+       ctx.moveTo(xd, yd);
+       ctx.bezierCurveTo(xd - 40, yd + 20, xd - 40, yd + 70, xd + 60, yd + 70);
+       ctx.bezierCurveTo(xd + 80, yd + 100, xd + 150, yd + 100, xd + 170, yd + 70);
+       ctx.bezierCurveTo(xd + 250, yd + 70, xd + 250, yd + 40, xd + 220, yd + 20);
+       ctx.bezierCurveTo(xd + 260, yd - 40, xd + 200, yd - 50, xd + 170, yd - 30);
+       ctx.bezierCurveTo(xd + 150, yd - 75, xd + 80, yd - 60, xd + 80, yd - 30);
+       ctx.bezierCurveTo(xd + 30, yd - 75, xd - 20, yd - 60, xd, yd);
+       ctx.fillStyle = "#24C000";
+       ctx.fill();
+       //ctx.stroke();
+       ctx.closePath();
+
+       var xe = 50;
+       var ye = 220;
+       ctx.beginPath();
+       ctx.lineWidth = 1;
+       ctx.moveTo(xe, ye);
+       ctx.bezierCurveTo(xe - 40, ye + 20, xe - 40, ye + 70, xe + 60, ye + 70);
+       ctx.bezierCurveTo(xe + 80, ye + 100, xe + 150, ye + 100, xe + 170, ye + 70);
+       ctx.bezierCurveTo(xe + 250, ye + 70, xe + 250, ye + 40, xe + 220, ye + 20);
+       ctx.bezierCurveTo(xe + 260, ye - 40, xe + 200, ye - 50, xe + 170, ye - 30);
+       ctx.bezierCurveTo(xe + 150, ye - 75, xe + 80, ye - 60, xe + 80, ye - 30);
+       ctx.bezierCurveTo(xe + 30, ye - 75, xe - 20, ye - 60, xe, ye);
+       ctx.fillStyle = "#24C000";
+       ctx.fill();
+       //ctx.stroke();
+       ctx.closePath();
+
+       var xf = 0;
+       var yf = 140;
+       ctx.beginPath();
+       ctx.lineWidth = 1;
+       ctx.moveTo(xf, yf);
+       ctx.bezierCurveTo(xf - 40, yf + 20, xf - 40, yf + 70, xf + 60, yf + 70);
+       ctx.bezierCurveTo(xf + 80, yf + 100, xf + 150, yf + 100, xf + 170, yf + 70);
+       ctx.bezierCurveTo(xf + 250, yf + 70, xf + 250, yf + 40, xf + 220, yf + 20);
+       ctx.bezierCurveTo(xf + 260, yf - 40, xf + 200, yf - 50, xf + 170, yf - 30);
+       ctx.bezierCurveTo(xf + 150, yf - 75, xf + 80, yf - 60, xf + 80, yf - 30);
+       ctx.bezierCurveTo(xf + 30, yf - 75, xf - 20, yf - 60, xf, yf);
+       ctx.fillStyle = "#24C000";
+       ctx.fill();
+       //ctx.stroke();
+       ctx.closePath();
+
+       //draw the cherries on the tree
+       ctx.beginPath();
+       ctx.arc(100, 150, 10, 0, 2 * Math.PI);
+       ctx.fillStyle = "red";
+       ctx.fill();
+       ctx.stroke();
+       ctx.closePath();
+
+       ctx.beginPath();
+       ctx.arc(130, 250, 10, 0, 2 * Math.PI);
+       ctx.fillStyle = "red";
+       ctx.fill();
+       ctx.stroke();
+       ctx.closePath();
+
+       ctx.beginPath();
+       ctx.arc(30, 210, 10, 0, 2 * Math.PI);
+       ctx.fillStyle = "red";
+       ctx.fill();
+       ctx.stroke();
+       ctx.closePath();
+
+       ctx.beginPath();
+       ctx.arc(210, 200, 10, 0, 2 * Math.PI);
+       ctx.fillStyle = "red";
+       ctx.fill();
+       ctx.stroke();
+       ctx.closePath();
+
+       requestAnimationFrame(drawBush);
+}
+
 function drawClouds() {
    //The first cloud
    ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -128,6 +233,7 @@ requestAnimationFrame(drawClouds);
 
 function draw() {
     drawClouds();
+    drawBush();
     drawBricks();
     drawHumptyDumpty();
 }
